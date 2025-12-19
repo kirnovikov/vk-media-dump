47f93ed97d2820d7021d88a711cae26101d0940d
kirnovikov <97402274+kirnovikov@users.noreply.github.com>
2025-12-20 02:59:56 +0600

    Create main.js

---
+++ electron/main.js
@@ -0,0 +1,67 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { spawn } = require('child_process');
+
+let mainWindow;
+let serverProcess;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    },
+    icon: path.join(__dirname, 'icon.png')
+  });
+
+  mainWindow.loadFile('index.html');
+}
+
+function startServer() {
+  const pythonScript = path.join(__dirname, 'server.py');
+  
+  serverProcess = spawn('python', ['-m', 'uvicorn', 'server:app', '--port', '8765', '--host', '127.0.0.1'], {
+    cwd: __dirname,
+    shell: true
+  });
+
+  serverProcess.stdout.on('data', (data) => {
+    console.log(`Server: ${data}`);
+  });
+
+  serverProcess.stderr.on('data', (data) => {
+    console.error(`Server Error: ${data}`);
+  });
+
+  serverProcess.on('close', (code) => {
+    console.log(`Server exited with code ${code}`);
+  });
+}
+
+app.whenReady().then(() => {
+  startServer();
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (serverProcess) {
+    serverProcess.kill();
+  }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('quit', () => {
+  if (serverProcess) {
+    serverProcess.kill();
+  }
+});
